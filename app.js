83e8ec9e5da6243707c703328c47e0d3d10eb522
marlonsalutia <marlon.muete@salutia.org>
2022-03-07 09:42:17 -0500

    Chat Bot con pruebas básicas

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const { Client } = require("whatsapp-web.js");
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 
-const SESSION_FILE_PATH = './sessions.js'
+const SESSION_FILE_PATH = './sessions.json'
 let sessionData;
 
 const countryCode = '57'
 const number = '3007300582'
-const msg = 'Buena tarde'
+const msg = 'Ingresa tu número de identifación'
 
 if(fs.existsSync(SESSION_FILE_PATH)) {
-  sessionData = require(SESSION_FILE_PATH)
+  sessionData = require(SESSION_FILE_PATH);
 }
 
 const client = new Client({
@@ -25,34 +25,37 @@ client.on("qr", (qr) => {
 client.on("ready", () => {
   console.log("Client is ready!");
 
-  let chatId = countryCode + number + '@c.us'
+  /*let chatId = countryCode + number + '@c.us'
   client.sendMessage(chatId, msg)
   .then(res => {
     if(res.id.fromMe){
       console.log('El mensaje fue enviado');
     }
-  })
+  })*/
 });
 
 client.on('authenticated', (session) => {
-  sessionData = session
+  sessionData = session;
 
   fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), err => {
     if(err){
       console.error(err)
     }
-  }) 
+  })
 })
 
 client.on('auth_failure', (msg) => {
   console.error('Hubo un fallo en autenticación', msg)
 })
 
-client.on("message", (msg) => {
-  if (msg.body == "!ping") {
-    client.searchMessages(msg.from, 'Pong')
-  }else if (msg.body == 'Hola'){
-    client.searchMessages(msg.from, 'Hola, buena tarde.')
+client.on('message', message => {
+  console.log(JSON.stringify(message));
+
+  // Cada ciudadano tiene su número de telefono
+	if(message.body === '!ping') {
+		client.sendMessage(message.from, 'pong');
+	} else if (message.body.toUpperCase() === 'TENGO PROBLEMAS') {
+    client.sendMessage(message.from.trim(), 'Llame a la línea amiga 😎');
   }
 });
 
